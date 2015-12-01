451712a4ad6c8730b7109267bc888efd4247e697
Rutger Farry <rutgerfarry@gmail.com>
2015-12-01 10:57:33 -0800

    Map view was way too wide, fixed with autolayout

--- classmereApp/TodayTableViewCell.swift
+++ classmereApp/TodayTableViewCell.swift
@@ -17,6 +17,24 @@ class TodayTableViewCell: AbstractClassmereCell {
     let timeLabel = UILabel()
     let buildingLabel = UILabel()
     
+    // MARK: - Map variables
+    
+    var pinLocation: MKPlacemark?
+    
+    let schoolZoomSpan = MKCoordinateSpan(
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02)
+    let buildingZoomSpan = MKCoordinateSpan(
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005)
+    
+    var buildingAddress: String?
+    var completedNavigation = false {
+        didSet {
+            completedNavigation = true
+        }
+    }
+    
     // MARK: - Layout subviews
     
     override func awakeFromNib() {
@@ -24,21 +42,21 @@ class TodayTableViewCell: AbstractClassmereCell {
         layoutTopSubviews()
         layoutMiddleSubviews()
         layoutBottomSubviews()
+        
+        formatTopSubViews()
     }
     
     func layoutTopSubviews() {
         let margins = aboveView.layoutMarginsGuide
+        aboveView.layoutMargins = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
         
         aboveView.addSubview(mapView)
+        
         mapView.translatesAutoresizingMaskIntoConstraints = false
         mapView.topAnchor.constraintEqualToAnchor(margins.topAnchor).active = true
         mapView.bottomAnchor.constraintEqualToAnchor(margins.bottomAnchor).active = true
         mapView.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor).active = true
         mapView.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor).active = true
-//        let origin = CGPoint(x: 0.0, y: 0.0)
-//        let size = aboveView.bounds.size
-//        let frame = CGRect(origin: origin, size: size)
-//        mapView.frame = frame
     }
     
     func layoutMiddleSubviews() {
@@ -52,13 +70,13 @@ class TodayTableViewCell: AbstractClassmereCell {
         
         middleView.addSubview(timeLabel)
         timeLabel.translatesAutoresizingMaskIntoConstraints = false
-        timeLabel.topAnchor.constraintEqualToAnchor(titleLabel.layoutMarginsGuide.bottomAnchor).active = true
+        timeLabel.topAnchor.constraintGreaterThanOrEqualToAnchor(titleLabel.layoutMarginsGuide.bottomAnchor, constant: 12.0).active = true
         timeLabel.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor).active = true
         timeLabel.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor).active = true
         
         middleView.addSubview(buildingLabel)
         buildingLabel.translatesAutoresizingMaskIntoConstraints = false
-        buildingLabel.topAnchor.constraintEqualToAnchor(timeLabel.layoutMarginsGuide.bottomAnchor).active = true
+        buildingLabel.topAnchor.constraintEqualToAnchor(timeLabel.layoutMarginsGuide.bottomAnchor, constant: 12.0).active = true
         buildingLabel.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor).active = true
         buildingLabel.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor).active = true
     }
@@ -66,12 +84,26 @@ class TodayTableViewCell: AbstractClassmereCell {
     func layoutBottomSubviews() {
         
     }
+    
+    // MARK: - Format Subviews
+    
+    func formatTopSubViews() {
+        let schoolCoordinates = CLLocationCoordinate2D(
+            latitude: 44.563849,
+            longitude: -123.279498)
+        let schoolCoordinateRegion = MKCoordinateRegion(
+            center: schoolCoordinates,
+            span: schoolZoomSpan)
+        mapView.setRegion(schoolCoordinateRegion, animated: false)
+    }
 
     func populateWithCourse(course: Course) {
         let section = course.courseSections[0]
         titleLabel.text = course.title?.capitalizedString
         timeLabel.text = TodayTableViewCell.formatCourseTime(course)
         buildingLabel.text = TodayTableViewCell.formatBuildingStringWithSection(section)
+        buildingAddress = section.building?.address
+        
         super.awakeFromNib()
     }
 
@@ -94,4 +126,39 @@ class TodayTableViewCell: AbstractClassmereCell {
             return "TBA"
         }
     }
+    
+    func startNavigationAnimation() {
+        if let buildingAddress = buildingAddress {
+            let geoCoder = CLGeocoder()
+            geoCoder.geocodeAddressString(buildingAddress) { placemarks, error in
+                if let placemark: CLPlacemark = placemarks![0],
+                    placemarkRegion = placemark.region as? CLCircularRegion {
+                        let mapKitPlacemark = MKPlacemark(placemark: placemark)
+                        var currentCoordinateRegion = self.mapView.region
+                        currentCoordinateRegion.center = placemarkRegion.center
+                        currentCoordinateRegion.span = self.buildingZoomSpan
+                        
+                        self.mapView.setRegion(currentCoordinateRegion, animated: true)
+                        self.mapView.addAnnotation(mapKitPlacemark)
+                        
+                        self.pinLocation = mapKitPlacemark
+                }
+            }
+        }
+    }
+    
+    override func cellDidExpand() {
+        if completedNavigation == false {
+            startNavigationAnimation()
+            completedNavigation = true
+        }
+    }
+    
+//    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+//        if let pinLocation = pinLocation {
+//            let mapItem = MKMapItem(placemark: pinLocation)
+//            mapItem.name = "Course Location"
+//            mapItem.openInMapsWithLaunchOptions(nil)
+//        }
+//    }
 }
